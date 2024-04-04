cb5b98c172adef206d016ffd50c8611e1c6aac0a
charissalow <160987980+charissalow@users.noreply.github.com>
2024-04-04 14:22:26 +1100

    Update OtherTableCreation.sql with Customer and Invoice

--- db/OtherTableCreation.sql
+++ db/OtherTableCreation.sql
@@ -4,7 +4,7 @@ CREATE TABLE AccessLog (
   LogDate DATE,
   LogDetails VARCHAR(30),
   CONSTRAINT AccessLog_PK PRIMARY KEY (LogID),
-  CONSTRAINT AccessLog_FK FOREIGN KEY (UserID) REFERENCES User(UserID))
+  CONSTRAINT AccessLog_FK FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
 
 CREATE TABLE Staff (
@@ -14,5 +14,27 @@ CREATE TABLE Staff (
   StaPhoneNo INT(10),
   StaEmail VARCHAR(20),
   RoleCode INT(15),
-  CONSTRAINT Staff_PK PRIMARY KEY (StaffID),
+  CONSTRAINT Staff_PK PRIMARY KEY (StaffID)
+);
+
+CREATE TABLE Customer (
+  CustomerID INT(5) NOT NULL,
+  CustFName VARCHAR(10),
+  CustLName VARCHAR(10),
+  CustPhoneNo INT(10),
+  CustEmail VARCHAR(20),
+  CustStreetAddress VARCHAR(20),
+  CustCity VARCHAR(20),
+  CustState VARCHAR(5),
+  CustPostCode INT(4),
+  CONSTRAINT Customer_PK PRIMARY KEY (CustomerID)
+);
+
+CREATE TABLE Invoice (
+  InvoiceID INT(5) NOT NULL,
+  CustomerID INT(5) NOT NULL,
+  InvoiceDate DATE,
+  InvoiceDueDate DATE,
+  CONSTRAINT Invoice_PK PRIMARY KEY (InvoiceID),
+  CONSTRAINT Invoice_FK FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
 );
