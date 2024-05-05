01d40ce7b8538e048fcf2bd692aca24f0e2b67d3
William Sinclair <160988772+9840234823@users.noreply.github.com>
2024-05-05 14:32:43 +1000

    Updated User.Java

--- src/java/isdmodel/User.java
+++ src/java/isdmodel/User.java
@@ -9,21 +9,51 @@ package isdmodel;
  * @author anna
  */
 public class User {
+    private Integer userID;
     private String name;
     private String email;
     private String password;
+    private Integer phoneNum;
+    private String address;
+    private String city; 
+    private String state;
+    private Integer postcode;
+    private Boolean activation;
 
-    public User(String name, String email, String password) {
+    public User(Integer userID, String name, String email, String password, Integer phoneNum, String address, String city, String state, Integer postcode, Boolean activation) {
+        this.userID = userID;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.phoneNum = phoneNum;
+        this.address = address; 
+        this.city = city; 
+        this.state = state; 
+        this.postcode = postcode; 
+        this.activation = activation;
     }
 
-    public String getemail() {
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
         return email;
     }
 
-    public void setemail(String email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 
@@ -35,13 +65,53 @@ public class User {
         this.password = password;
     }
 
-    public String getName() {
-        return name;
+    public Integer getPhoneNum() {
+        return phoneNum;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPhoneNum(Integer phoneNum) {
+        this.phoneNum = phoneNum;
     }
-        
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(Integer postcode) {
+        this.postcode = postcode;
+    }
+
+    public Boolean getActivation() {
+        return activation;
+    }
+
+    public void setActivation(Boolean activation) {
+        this.activation = activation;
+    }
+
 
 }
