7f80bc27f6edd1b98474f038dd35becc927ab030
elluhhh <ella.r.gibbs@student.uts.edu.au>
2024-05-15 01:13:25 +1000

    doGet for main.jsp -> payment.jsp + name and cardNo validation working

--- src/java/isdcontroller/PaymentMethodServlet.java
+++ src/java/isdcontroller/PaymentMethodServlet.java
@@ -13,34 +13,69 @@ package isdcontroller;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import isdmodeldao.PaymentMethodDBManager;
 import isdmodel.*;
+import isdmodeldao.DBManager;
 
-public class PaymentMethodServlet {
+public class PaymentMethodServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+        HttpSession session = request.getSession();
+        int userID = Integer.parseInt(request.getParameter("userID"));
+        DBManager manager = (DBManager) session.getAttribute("manager");
+        PaymentMethod paymentMethod = null;
+        
+        try{
+            if(manager.checkPaymentMethod(userID)){
+                paymentMethod = manager.findPaymentMethod(userID);
+                session.setAttribute("paymentMethod", paymentMethod);
+            }
+        } catch (SQLException ex){
+            Logger.getLogger(PaymentMethodServlet.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getErrorCode() + " and " + ex.getMessage());
+        }
+         request.getRequestDispatcher("payment.jsp").include(request, response);
+    }
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         Validator validator = new Validator();
+        
+        //name = parameter, not id
+        String cardName = request.getParameter("cardName");
         String cardNo = request.getParameter("cardNo");
         String expiryDate = request.getParameter("expiryDate");
         String cvv = request.getParameter("cvv");
-        PaymentMethodDBManager manager = (PaymentMethodDBManager) session.getAttribute("manager");
-        PaymentMethod paymentMethod = null;
+        
+        DBManager manager = (DBManager) session.getAttribute("manager");
+        User user = (User) session.getAttribute("user");
+        int userID = user.getUserID();
         validator.clear(session);
         
-        if(!validator.validateCardNo(cardNo)){
+       
+        if(!validator.validateName(cardName)){
+            session.setAttribute("nameErr", "Error: Card Name format incorrect");
+            request.getRequestDispatcher("payment.jsp").include(request, response);
+        }
+        else if (!validator.validateCardNo(cardNo)){
             session.setAttribute("cardNoErr", "Error: Card Number format incorrect");
             request.getRequestDispatcher("payment.jsp").include(request, response);
-        } else if (!validator.validateExpiryDate(expiryDate)){
+        }
+        else if (!validator.validateExpiryDate(expiryDate)){
             session.setAttribute("expiryDateErr", "Error: Card Expiry Date format incorrect");
             request.getRequestDispatcher("payment.jsp").include(request, response);
-        } else if (!validator.validateCVV(cvv)){
+        }
+        else if (!validator.validateCVV(cvv)){
             session.setAttribute("cvvErr", "Error: Card CVV format incorrect");
             request.getRequestDispatcher("payment.jsp").include(request, response);
         }
