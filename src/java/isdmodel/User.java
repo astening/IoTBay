db4d1ac09c25162a928f2c13e57edcb4c2c257c9
elluhhh <ella.r.gibbs@student.uts.edu.au>
2024-05-15 01:17:26 +1000

    added userID var

--- src/java/isdmodel/User.java
+++ src/java/isdmodel/User.java
@@ -9,21 +9,31 @@ package isdmodel;
  * @author anna
  */
 public class User {
+    private int userID;
     private String name;
     private String email;
     private String password;
 
-    public User(String name, String email, String password) {
+    public User(int userID, String name, String email, String password) {
+        this.userID = userID;
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
-    public String getemail() {
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
         return email;
     }
 
-    public void setemail(String email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 
