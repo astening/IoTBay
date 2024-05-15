1363c0239a511ecff21864ebd51ed2c6c2838e5c
charissalow <charissa.low@student.uts.edu.au>
2024-05-15 16:42:59 +1000

    Update servlet with addOrder
    Only taking default values
    Number validation not working

--- src/java/isdcontroller/OrderFormServlet.java
+++ src/java/isdcontroller/OrderFormServlet.java
@@ -4,8 +4,7 @@
  */
 package isdcontroller;
 
-import isdmodeldao.DBConnector;
-import isdmodeldao.DBManager;
+import isdmodeldao.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -39,6 +38,25 @@ public class OrderFormServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+       
+    @Override //Create an instance of DBConnector for the deployment session
+
+       public void init() {
+
+           try {
+
+               db = new DBConnector();
+
+           } catch (ClassNotFoundException | SQLException ex) {
+
+               Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex);
+
+           }      
+
+       }
+       
+       
+       
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -72,21 +90,21 @@ public class OrderFormServlet extends HttpServlet {
 
            HttpSession session = request.getSession();
 
-           conn = db.openConnection();       
-
-           try {
-
-               manager = new DBManager(conn);
-
-           } catch (SQLException ex) {
-
-               Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex);
-
-           }
+//           conn = db.openConnection();       
+//
+//           try {
+//
+//               manager = new DBManager(conn);
+//
+//           } catch (SQLException ex) {
+//
+//               Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex);
+//
+//           }
 
            //export the DB manager to the view-session (JSPs)
 
-           session.setAttribute("manager", manager);   
+//           session.setAttribute("manager", manager);   
         processRequest(request, response);
     }
 
@@ -101,8 +119,68 @@ public class OrderFormServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.setAttribute("updated", true) ;
-//        processRequest(request, response);
+        request.setAttribute("updated", "Order was not submitted") ;
+        
+        // Retrieve session from request
+        HttpSession session = request.getSession() ;
+        
+        // Set up a validator object for the session and clear variables
+        Validator validator = new Validator() ;
+        validator.clear(session) ;
+        
+        // Retrieve posted data
+//        String productID = (String) session.getAttribute("productID") ; // is null --> what happens if i make this an int
+        String productID = "1" ;
+        int intProductID = 0 ;
+        int intItemQuantity = 1 ;
+//        int intItemQuantity = (int) session.getAttribute("itemQuantity") ;
+        String itemQuantity = (String) session.getAttribute("itemQuantity") ; // unaccepted
+        
+        // add and create db manager
+        conn = db.openConnection() ;       
+
+        try {
+
+            manager = new DBManager(conn) ;
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex) ;
+
+        }
+        
+        // Check variables are valid before using manager to perform update
+        if(validator.checkEmpty(2, productID)) {
+            session.setAttribute("productID", "Please provide a product ID") ;
+        }
+        else if (validator.checkEmpty(2, itemQuantity)) {
+            session.setAttribute("itemQuantity", "Please provide a product ID") ;
+        }
+//        else if (!validator.validateNumber(itemQuantity)) {
+//            session.setAttribute("IDValidated", "Please enter a valid quantity number above 0") ;
+//        }
+        else if (!validator.validateNumber(productID)) {
+            session.setAttribute("IDValidated", "Please provide a valid product ID") ;
+        }
+        else {
+            intProductID = Integer.parseInt(productID) ;
+        }
+        
+        // Use manager to add the order into the database
+
+           try {
+//               manager.updateOrder(2, intProductID, intItemQuantity) ; // temporary fix
+            int userID = 1 ;
+            int orderID = 25 ;
+            manager.addOrder(orderID, intItemQuantity, intProductID, userID);
+               // set confirmation message for product id, quantity
+               session.setAttribute("updated", "Order was submitted") ;
+               
+               // provide user with the order number and date
+           } catch (SQLException ex) {
+               Logger.getLogger(OrderFormServlet.class.getName()).log(Level.SEVERE, null, ex) ;
+           }
+           
         request.getRequestDispatcher("orderForm.jsp").include(request, response) ;
     }
 
@@ -113,7 +191,7 @@ public class OrderFormServlet extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+        return "Short description" ;
     }// </editor-fold>
 
 }
